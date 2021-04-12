6c940e7308d7115d20fcd824a7d2f2906bb53881
noaherzhang <noaherzhang@tencent.com>
2021-04-12 13:12:51 +0800

    1

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ function shouldCompress(req, res) {
 
 app.use(
   compression({
-    // demo1
     level: 2, // set compression level from 1 to 9 (6 by default)
     filter: shouldCompress // set predicate to determine whether to compress
   })
